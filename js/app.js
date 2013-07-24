6ea9137e4b548671a4fc84410af07d6f04f694a7
Matt Hodges <hodgesmr1@gmail.com>
2013-07-23 22:52:41 -0400

    a little bit of restructuring

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 App = Ember.Application.create();
 
 App.ExternalLinks = [];
+App.ExternalLink = Ember.Object.create();
 
-App.ExternalLink = Ember.Object.create({
-	title: "",
-	url: ""
-});
+App.Posts = [];
+App.Post = Ember.Object.create();
 
-$.getJSON('externalLinks.json', function(data) {
+App.PostBody = Ember.Object.create();
+
+$.getJSON('./content/externalLinks.json', function(data) {
 	App.set("ExternalLinks",data);
 })
 .fail(function() { console.error("Error loading externalLinks.json"); });
