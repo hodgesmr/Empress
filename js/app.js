8d35d584272e9469cd3a69c45f6e7ded152c782e
Matt Hodges <hodgesmr1@gmail.com>
2013-07-23 22:42:22 -0400

    ember-data didn't fulfill my hopes and dreams, so I got rid of it

--- js/app.js
+++ js/app.js
@@ -1,52 +1,18 @@
 App = Ember.Application.create();
 
-var externalLinksJSON = [{
-	id: 0
-}];
+App.ExternalLinks = [];
+
+App.ExternalLink = Ember.Object.create({
+	title: "",
+	url: ""
+});
 
 $.getJSON('externalLinks.json', function(data) {
-	externalLinksJSON = data;
-	console.log(externalLinksJSON);
+	App.set("ExternalLinks",data);
 })
-.fail(function() { console.error("There was an error loading externalLinks.json"); });
-
-// The above successfully updates the externalLinksJSON variable
-// but Ember has already rendered the page with the original JSON data
-// Below will do everything synchronously, but that's gross...
-
-/*
-$.ajax({
-	url:'externalLinks.json',
-	async: false,
-	dataType: 'json',
-	success: function(data) {
-		externalLinksJSON = data;
-	},
-	error: function() {
-		console.log.error("Error loading externalLinks.json")
-	}
-});
-*/
-
-App.Store = DS.Store.extend({
-	revision: 12,
-	adapter: 'DS.FixtureAdapter'
-});
+.fail(function() { console.error("Error loading externalLinks.json"); });
 
 App.Router.map(function() {
 	this.resource('home');
 	this.resource('about');
 });
-
-App.HomeRoute = Ember.Route.extend({
-	model: function() {
-		return App.ExternalLink.find();
-	}
-});
-
-App.ExternalLink = DS.Model.extend({
-	title: DS.attr('string'),
-	url: DS.attr('string')
-});
-
-App.ExternalLink.FIXTURES = externalLinksJSON;
